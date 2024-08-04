7b136fa47ac61d6940a5ee0b89e10a5334b070dc
SeaofChaos <46305438+ryqui@users.noreply.github.com>
2024-08-03 20:41:17 -0400

    Added ROC AUC calculation to GNN

--- utils_classification/GraphConvolutionalNetwork.py
+++ utils_classification/GraphConvolutionalNetwork.py
@@ -1,8 +1,11 @@
 import torch
+import numpy as np
+from sklearn.metrics import roc_auc_score, roc_curve
 from torch_geometric.nn import GCNConv, GCN
 from torch_geometric.data import DataLoader
 from torch.nn import CrossEntropyLoss
 from torch.optim import Adam
+import matplotlib.pyplot as plt
 
 
 class GraphConvolutionalNetwork:
@@ -42,3 +45,33 @@ class GraphConvolutionalNetwork:
         accuracy = correct / total_samples
         print(f'Test Loss: {total_loss/len(loader):.4f}, Accuracy: {accuracy:.4f}')
         return total_loss / len(loader), accuracy
+    
+    def calculate_roc_auc(self, loader, plot=False):
+        self.model.eval()
+        y_true = []
+        y_pred_prob = []
+
+        with torch.no_grad():
+            for data in loader:
+                out = self.model(data.x, data.edge_index)
+                y_true.extend(data.y[data.test_mask].cpu().numpy())
+                y_pred_prob.extend(out[data.test_mask][:, 1].cpu().numpy())
+
+        y_true = np.array(y_true)
+        y_pred_prob = np.array(y_pred_prob)
+        roc_auc = roc_auc_score(y_true, y_pred_prob)
+
+        if plot:
+            fpr, tpr, _ = roc_curve(y_true, y_pred_prob)
+            plt.figure()
+            plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+            plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+            plt.xlim([0.0, 1.0])
+            plt.ylim([0.0, 1.05])
+            plt.xlabel('False Positive Rate')
+            plt.ylabel('True Positive Rate')
+            plt.title('Receiver Operating Characteristic')
+            plt.legend(loc="lower right")
+            plt.show()
+
+        return roc_auc
